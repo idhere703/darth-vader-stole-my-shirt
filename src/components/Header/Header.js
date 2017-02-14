91a329022df9c648848d71acf2975a32b72fc15d
Brandon <idhere703@gmail.com>
2017-02-13 19:33:03 -0500

    Header addition.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.css';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div className="App-header">
+        <div className="App-header-cont">
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import logo from '../../logo.svg';
 import './Page.css'
 
 class Page extends Component {
   render() {
     return (
-      <div className="vertical-container">
-        <div className="vertical-box">
-          Content with flex.
-        </div>
-        <div className="vertical-box">
-          Other.
-        </div>
-        <div className="vertical-box">
-          And some more.
+      <div className="content">
+        <div className="vertical-container">
+          <div className="vertical-box">
+            Content with flex.
+          </div>
+          <div className="vertical-box">
+            Other.
+          </div>
+          <div className="vertical-box">
+            And some more.
+          </div>
         </div>
       </div>
     );
