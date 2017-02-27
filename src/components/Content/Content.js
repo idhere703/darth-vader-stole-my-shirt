a1f01408db34ffc3eb18228479381b10eab54ad1
Brandon <idhere703@gmail.com>
2017-02-27 17:47:46 -0500

    Content for the page.

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './Content.css';
 
 class Content extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <div className="Content">
-        {props.children}
+        {this.props.children}
       </div>
     );
   }
