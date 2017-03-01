953cd15908c236e3130104ff68662da9e0b8158a
Brandon <idhere703@gmail.com>
2017-02-28 21:47:07 -0500

    Mocking data services.

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Page.css'
 import Content from '../Content/Content';
+import { lines } from '../../data/text';
 
 class Page extends Component {
   render() {
@@ -8,24 +9,13 @@ class Page extends Component {
       <Content>
         <div className="content">
           <div className="vertical-container">
-            <div className="vertical-box">
-              Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the I listened for the sound of their voices, but not a murmur passed through the curtain which I had drawn across the entrance to the smoking cabin.  Only a peculiar tremor from the mysterious engines broke the utter stillness.  Was I growing deaf?  I snapped my fingers to reassure myself, and the sound startled me like the crack of a pistol.  Evidently my sense of hearing had become abnormally acute.  My mind, too, was preternaturally clear, and the solitude became so irksome that I rose from my seat, and looked out of the scuttles to relieve the tension of my nerves.
-            </div>
-            <div className="vertical-box">
-              Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.  By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside, the sable cloud beneath was dished out, and the car seemed to float in the middle of an immense dark sphere, whose upper half was strewn with silver.  Looking down into the dark gulf below, I could see a ruddy light streaming through a rift in the clouds.  It was probably a last glimpse of London, or some neighbouring town; but soon the rolling vapours closed, and shut it out.
-            </div>
-            <div className="vertical-box">
-              I now realised to the full that I was nowhere, or to speak more correctly, a wanderer in empty space—­that I had left one world behind me and was travelling to another, like a disembodied spirit crossing the gloomy Styx.  A strange serenity took possession of my soul, and all that had polluted or degraded it in the lower life seemed to fall away from it like the shadow of an evil dream.
-            </div>
-            <div className="vertical-box">
-              In the depths of my heart I no longer felt sorry to quit the earth.  It seemed to me now, a place where the loveliest things never come to birth, or die the soonest—­where life itself hangs on a blind mischance, where true friendship is afraid to show its face, where pure love is unrequited or betrayed, and the noblest benefactors of their fellowmen have been reviled or done to death—­a place which we regard as a heaven when we enter it, and a hell before we leave it. . . .  No, I was not sorry to quit the earth.
-            </div>
-            <div className="vertical-box">
-              And the beautiful planet, shining there so peacefully in the west, was it any better?  At a like distance the earth would seem still fairer, and perhaps even now some wretch in Venus is asking himself a similar question.  Is it not probable that just as all the worlds are made of the same materials, so the mixture of good and evil is much the same in all?  I turned to the stars, where in all ages man has sought an answer to his riddles.  The better land!  Where is it? if not among the stars.  I am now in the old heaven above the clouds.  Does it lie within the visible universe, as it lies within the heart when peace and happiness are there?
-            </div>
-            <div className="vertical-box">
-              In that pure ether the glory of the firmament was revealed to me as it had never been on the earth, where it is often veiled with clouds and mist, or marred by houses and surrounding objects—­where the quietude of the mind is also apt to be disturbed by sordid and perplexing cares.  Its awful sublimity overwhelmed my faculties, and its majesty inspired me with a kind of dread.  In presence of these countless orbs my own nothingness came home to me, and a voice seemed to whisper in my ear,
-            </div>
+            {lines.map((text) => {
+              return (
+                <div className="vertical-box">
+                  {text}
+                </div>
+              );
+            })}
           </div>
         </div>
       </Content>
