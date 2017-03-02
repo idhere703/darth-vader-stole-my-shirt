8911e1aeeb19cffc0be5c759c0812eb5f0d895bb
Brandon <idhere703@gmail.com>
2017-03-01 21:55:33 -0500

    FB icon add.

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -2,9 +2,6 @@ import React from 'react';
 import './Content.css';
 
 class Content extends React.Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return (
       <div className="Content">
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,6 +8,9 @@ class Header extends React.Component {
         <div className="App-header-cont">
           <h1>Darth Vader Stole My Shirt</h1>
         </div>
+        <div className="App-header-social">
+          <i className="fa fa-facebook-square" aria-hidden="true"></i>
+        </div>
       </div>
     );
   }
