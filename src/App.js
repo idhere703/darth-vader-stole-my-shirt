e1583dcee0be7ea2427abafe43677896915f2fcf
Brandon <idhere703@gmail.com>
2017-03-03 22:28:30 -0500

    I don't know about this sidebar.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Page from './components/Page/Page';
 import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header></Header>
+        <Sidebar></Sidebar>
         <div className="App">
           <Page></Page>
         </div>
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Sidebar.css'
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <div className="Sidebar">
+        Content.
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
