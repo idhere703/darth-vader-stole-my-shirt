59fd7a4cb937788c828a601debd8102580e18b60
Brandon <idhere703@gmail.com>
2017-03-18 19:45:10 -0400

    Calendar html. Basic.

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -3,9 +3,31 @@ import React, { Component } from 'react';
 export class Calendar extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <diV>
+        {this.props.month &&
+          <table>
+            <thead>
+              <th>Sun</th>
+              <th>Mon</th>
+              <th>Tue</th>
+              <th>Wed</th>
+              <th>Thu</th>
+              <th>Fri</th>
+              <th>Sat</th>
+            </thead>
+
+            <tbody>
+                {this.props.weeks && this.props.weeks.map((week) => {
+                  return(<tr>
+                    {week.days && week.days.map((day) => {return (
+                      <td id={day.id}>{day.day}</td>
+                    );})}
+                  </tr>);
+                })}
+            </tbody>
+          </table>
+        }
+      </diV>
     );
   };
 }
