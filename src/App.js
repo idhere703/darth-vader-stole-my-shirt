6acd9b5fcb9713f4ea0d387979aad1d3e1809d79
Brandon <idhere703@gmail.com>
2017-11-21 21:46:55 -0500

    Cleared old code away.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import Page from './components/Page/Page';
-import Header from './components/Header/Header';
-import Sidebar from './components/Sidebar/Sidebar';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <Header></Header>
-        <Sidebar></Sidebar>
         <div id="App" className="App">
-          <Page></Page>
         </div>
       </div>
 
-    );
+      );
   }
 }
 
--- src/data/socialMediaLinks.js
+++
@@ -1,7 +0,0 @@
-export default {
-  facebook: 'https://www.facebook.com',
-  github: 'https://github.com/',
-  instagram: 'https://instagram.com/',
-  twitter: 'https://twitter.com/',
-  snapchat: 'https://snapchat.com/'
-};
