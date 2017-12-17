ecdcc9be93a8617a4c009fd315b9d1e18e2d1e67
Brandon <idhere703@gmail.com>
2017-12-16 21:33:49 -0500

    Arrow key support.

--- src/components/utils.js
+++ src/components/utils.js
@@ -19,4 +19,22 @@ utils.watchForSpacebar = (target, fn) => {
     });
 };
 
+utils.watchForArrowKeys = (target, fn) => {
+    // left = 37
+    // up = 38
+    // right = 39
+    // down = 40
+    $(target).keyup((evt) => {
+        if (evt.keyCode === 37) {
+            fn(evt)
+        } else if (evt.keyCode === 38) {
+            fn(evt)
+        } else if (evt.keyCode === 39) {
+            fn(evt)
+        } else if (evt.keyCode === 40) {
+            fn(evt)
+        }
+    });
+};
+
 export default utils;
